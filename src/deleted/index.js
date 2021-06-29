271b143516f5a5438876638126fca16f33bda15a
rafael.monteiro <rafael.monteiro@segfy.com>
2021-06-29 08:57:11 -0300

    Treating array if deleted item and updated item

--- src/deleted/index.js
+++ src/deleted/index.js
@@ -7,13 +7,26 @@ const deletedDiff = (lhs, rhs) => {
   const r = properObject(rhs);
 
   return Object.keys(l).reduce((acc, key) => {
-    if (r.hasOwnProperty(key)) {
+    if (l.hasOwnProperty(key) && !Array.isArray(l) && !Array.isArray(r)) {
       const difference = deletedDiff(l[key], r[key]);
 
       if (isObject(difference) && isEmpty(difference)) return acc;
 
       return { ...acc, [key]: difference };
     }
+    else if (Array.isArray(l) && Array.isArray(r)) {
+      const rId = r[key].id;
+
+      const lItem = l.find((i) => i.id === rId);
+
+      if(!lItem) return acc;
+
+      const difference = updatedDiff(lItem, r[key]);
+
+      if (isObject(difference) && isEmpty(difference) && !isDate(difference)) return acc;
+
+      return { ...acc, [key]: {...difference, id: rId} };
+  }
 
     return { ...acc, [key]: undefined };
   }, {});
--- src/updated/index.js
+++ src/updated/index.js
@@ -16,13 +16,26 @@ const updatedDiff = (lhs, rhs) => {
 
   return Object.keys(r).reduce((acc, key) => {
 
-    if (l.hasOwnProperty(key)) {
+    if (l.hasOwnProperty(key) && !Array.isArray(l) && !Array.isArray(r)) {
       const difference = updatedDiff(l[key], r[key]);
 
       if (isObject(difference) && isEmpty(difference) && !isDate(difference)) return acc;
 
       return { ...acc, [key]: difference };
     }
+    else if (Array.isArray(l) && Array.isArray(r)) {
+      const rId = r[key].id;
+
+      const lItem = l.find((i) => i.id === rId);
+
+      if(!lItem) return acc;
+
+      const difference = updatedDiff(lItem, r[key]);
+
+      if (isObject(difference) && isEmpty(difference) && !isDate(difference)) return acc;
+
+      return { ...acc, [key]: {...difference, id: rId} };
+  }
 
     return acc;
   }, {});
